945cf77c95c2f2e0904f74981577241a0e2d68b7
Qrikko <qrikko@gmail.com>
2020-04-10 11:34:08 +0200

    renaming, added a debug script and some polish

---
+++ Assets/Scripts/DebugInfo.cs
@@ -0,0 +1,31 @@
+﻿/* This script is only meant for debugging reasons, to help keep track of
+    things like IDs and hashed values since it's a bit of a pain to handle them.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DebugInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void print_debug() {
+        Debug.Log("active game: " + PlayerPrefs.GetString("userid"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) {
+            print_debug();
+        }
+        
+    }
+}
