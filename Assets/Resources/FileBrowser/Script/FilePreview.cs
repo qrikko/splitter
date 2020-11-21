3c4ddf5e8bec41b6078558bc37aaeafc9cb4997d
Qrikko <qrikko@gmail.com>
2020-11-21 19:23:41 +0100

    implemented size slider

---
+++ Assets/Resources/FileBrowser/Script/FilePreview.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FilePreview : MonoBehaviour
+{
+    private GridLayoutGroup _layout;
+    
+    public void set_cell_size(float size) {
+        _layout.cellSize = new Vector2(size, size);
+    }
+
+    void Start() {
+        _layout = GetComponent<GridLayoutGroup>();
+    }
+}
